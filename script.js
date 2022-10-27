5a558540556adfa0a07934f8335916effe8d05e0
BANOTH ADHVAITHA NAYAK <84926877+Banoth-Adhvaitha-Nayak@users.noreply.github.com>
2022-10-27 11:04:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+  };
+
+  const serviceID = "service_igg96vt";
+  const templateID = "template_matffym";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
